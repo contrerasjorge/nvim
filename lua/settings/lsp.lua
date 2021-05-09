00b4a749513f09ff93e4615367ad3dbccf135620
Jorge Contreras <jorge.contreras789@gmail.com>
2021-05-09 14:32:28 -0400

    Elm ... 多分無理だろう

--- lua/settings/lsp.lua
+++ lua/settings/lsp.lua
@@ -95,9 +95,8 @@ M.setup = function()
 	--})
 	--lsp_config.yamlls.setup({})
 	--lsp_config.racket_langserver.setup({})
-  --
-  lsp_config.html.setup({
-  })
+  lsp_config.elmls.setup{}
+  lsp_config.html.setup({})
   lsp_config.cssls.setup({})
   lsp_config.pyright.setup({})
   lsp_config.rust_analyzer.setup({})
