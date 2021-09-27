5d00bcdb81780a1608c8879917a4efe78a960d92
Jorge Contreras <jorge.contreras789@gmail.com>
2021-09-26 23:21:27 -0400

    Remove unused settings and update ignored patterns for treesitter

--- init.lua
+++ init.lua
@@ -364,12 +364,10 @@ require("onedark").setup({
 })
 
 -- cmd("colorscheme nightfly")
-cmd([[let g:gruvbox_contrast_dark = 'hard']])
 cmd([[if exists('+termguicolors')
   let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
   let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
 endif]])
-cmd([[let ayucolor="mirage"]])
 
 -- Colors galore
 require("colorizer").setup()
