945154d03cf64e35f0d2e85f147b0587f234603a
Jorge Contreras <jorge.contreras789@gmail.com>
2023-10-02 19:30:16 -0400

    Removed commented out code

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,3 @@
--- require('colorbuddy').colorscheme('gruvbuddy')
--- vim.cmd.colorscheme "catppuccin"
 require('colorizer').setup()
 require("catppuccin").setup({
     flavour = "macchiato", -- latte, frappe, macchiato, mocha
@@ -7,15 +5,3 @@ require("catppuccin").setup({
 
 -- setup must be called before loading
 vim.cmd.colorscheme "catppuccin"
-
-
--- function ColorMyPencils(color)
--- 	color = color or "rose-pine"
--- 	vim.cmd.colorscheme(color)
---
--- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- 	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
--- end
---
--- ColorMyPencils()
